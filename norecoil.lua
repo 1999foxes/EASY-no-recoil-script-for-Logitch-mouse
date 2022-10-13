eac49a8f9055ffc464018b4c3fb35a7e41c39ec0
1999foxes <hl1999@yeah.net>
2022-10-14 01:51:58 +0800

    Update norecoil.lua

--- norecoil.lua
+++ norecoil.lua
@@ -1,15 +1,16 @@
 EnablePrimaryMouseButtonEvents(true)
 
-step = 2  -- distance of mouse movements 压枪幅度
-slp = 6   -- time interval of mouse movements 压枪间隔
+step = 2  -- distance of mouse movements in pixel
+slp = 6   -- time interval of mouse movements in milliseconds
 
 function OnEvent(event, arg)
     OutputLogMessage("event = %s, arg = %d\n", event, arg)
 
-    if (event == "MOUSE_BUTTON_PRESSED" and arg == 1) then
-        repeat
+    if (event == "MOUSE_BUTTON_PRESSED" and arg == 1) then          -- left mouse pressed event
+        while IsMouseButtonPressed(3) and IsMouseButtonPressed(1)   -- reduce recoil while left and right mouse buttons are pressed
+        do
             MoveMouseRelative(0, step)
             Sleep(slp)
-        until not IsMouseButtonPressed(3) or not IsMouseButtonPressed(1)
+        end
     end
 end
