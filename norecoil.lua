5315875d8e59171c664c01f8b0d954c8de9f5aa7
1999foxes <hl1999@yeah.net>
2022-10-14 01:54:01 +0800

    Update norecoil.lua

--- norecoil.lua
+++ norecoil.lua
@@ -4,7 +4,7 @@ step = 2  -- distance of mouse movements in pixel
 slp = 6   -- time interval of mouse movements in milliseconds
 
 function OnEvent(event, arg)
-    OutputLogMessage("event = %s, arg = %d\n", event, arg)
+    -- OutputLogMessage("event = %s, arg = %d\n", event, arg)       -- for 
 
     if (event == "MOUSE_BUTTON_PRESSED" and arg == 1) then          -- left mouse pressed event
         while IsMouseButtonPressed(3) and IsMouseButtonPressed(1)   -- reduce recoil while left and right mouse buttons are pressed
